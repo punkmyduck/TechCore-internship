b8f99d673107d8b7c21f42be67b7596616f835db
punkmyduck <punkmyduck@mail.ru>
2025-10-23 14:19:45 +0300

    ExceptionHandlerMiddleware added

---
+++ Infrastructure/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace task_1135.Infrastructure.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/problem+json";
+
+                var problem = new ProblemDetails
+                {
+                    Status = 500,
+                    Title = "Internal server error",
+                    Detail = ex.Message,
+                    Instance = context.Request.Path
+                };
+
+                await context.Response.WriteAsJsonAsync(problem);
+            }
+        }
+    }
+}
