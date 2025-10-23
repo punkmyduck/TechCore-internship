7c107baa7dd5eeb8f6239bbfd5e122fa4046be56
punkmyduck <punkmyduck@mail.ru>
2025-10-23 15:32:50 +0300

    Configuration registered + added controller for appsettings testing

---
+++ Presentation/Controllers/AppSettingsTestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using task_1135.Application.Settings;
+
+namespace task_1135.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppSettingsTestController : ControllerBase
+    {
+        private readonly MySettings _settings;
+        public AppSettingsTestController(IOptions<MySettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                _settings.ApiKey,
+                _settings.MaxItems
+            });
+        }
+    }
+}
