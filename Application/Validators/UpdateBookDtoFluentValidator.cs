9dbd063aab9614a50c3df5fed0eac72c71029925
punkmyduck <punkmyduck@mail.ru>
2025-10-23 14:38:01 +0300

    UpdateBookDto fluent validation added

---
+++ Application/Validators/UpdateBookDtoFluentValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using task_1135.Application.DTOs;
+
+namespace task_1135.Application.Validators
+{
+    public class UpdateBookDtoFluentValidator : AbstractValidator<UpdateBookDto>
+    {
+        public UpdateBookDtoFluentValidator()
+        {
+            RuleFor(updateBookDto => updateBookDto.Title)
+                .NotEmpty()
+                .MaximumLength(250);
+
+            RuleFor(updateBookDto => updateBookDto.YearPublished)
+                .Must(b => (b > 1900) && b <= (DateTime.Now.Year));
+        }
+    }
+}
