c11785129dc516fd36beb42713fb59a25e86e922
punkmyduck <punkmyduck@mail.ru>
2025-10-23 11:06:13 +0300

    hello world controller added

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace task_1135.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet(Name = "Hello")]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Hello, World!" });
+        }
+    }
+}
