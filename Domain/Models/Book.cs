8d63598e74ef0084ec961c9ca84aa3ea4c4faf67
punkmyduck <punkmyduck@mail.ru>
2025-10-24 13:00:54 +0300

    Исправлены предупреждения для класса Book

--- Domain/Models/Book.cs
+++ Domain/Models/Book.cs
@@ -3,9 +3,9 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } = null!;
         public int YearPublished { get; set; }
         public int AuthorId { get; set; }
-        public Author Author { get; set; }
+        public Author Author { get; set; } = null!;
     }
 }
