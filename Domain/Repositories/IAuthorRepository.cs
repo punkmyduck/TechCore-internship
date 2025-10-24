d9c6412b5133af00ad96fd5e622a08c00cca860f
punkmyduck <punkmyduck@mail.ru>
2025-10-24 13:50:35 +0300

    Добавлен репозиторий для управления авторами

---
+++ Domain/Repositories/IAuthorRepository.cs
@@ -0,0 +1,12 @@
+﻿using task_1135.Domain.Models;
+
+namespace task_1135.Domain.Repositories
+{
+    public interface IAuthorRepository
+    {
+        Task Add(Author author);
+        Task<Author?> GetById(int id);
+        Task<IEnumerable<Author>> GetAll();
+        Task DeleteById(int id);
+    }
+}
---
+++ Infrastructure/Repositories/DatabaseAuthorRepository.cs
@@ -0,0 +1,28 @@
+﻿using task_1135.Domain.Models;
+using task_1135.Domain.Repositories;
+
+namespace task_1135.Infrastructure.Repositories
+{
+    public class DatabaseAuthorRepository : IAuthorRepository
+    {
+        public Task Add(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Author>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Author?> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
