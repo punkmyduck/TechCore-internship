55045419a12872e66c164843274579dd9484a4c4
punkmyduck <punkmyduck@mail.ru>
2025-10-24 13:45:27 +0300

    Добавлен контроллер для управления авторами

---
+++ Presentation/Controllers/AuthorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using task_1135.Application.DTOs;
+
+namespace task_1135.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAuthors()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAuthorById([FromRoute] int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAuthor([FromBody] CreateAuthorDto createAuthorDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor([FromRoute] int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
