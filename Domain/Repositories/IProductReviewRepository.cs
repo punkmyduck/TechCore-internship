2c0236eef51fc88f0c65d62540afb6ac25370a8c
punkmyduck <punkmyduck@mail.ru>
2025-10-27 14:50:27 +0300

    Добавлена модель и репозиторий для работы с отзывами

---
+++ Domain/Repositories/IProductReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using task_1135.Domain.Models;
+
+namespace task_1135.Domain.Repositories
+{
+    public interface IProductReviewRepository
+    {
+        Task<IEnumerable<ProductReview>> GetAllAsync();
+        Task<ProductReview?> GetByIdAsync(string id);
+        Task AddAsync(ProductReview productReview);
+        Task UpdateAsync(ProductReview productReview);
+        Task DeleteAsync(string id);
+    }
+}
---
+++ Infrastructure/Repositories/ProductReviewRepository.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Driver;
+using task_1135.Domain.Models;
+using task_1135.Domain.Repositories;
+
+namespace task_1135.Infrastructure.Repositories
+{
+    public class ProductReviewRepository : IProductReviewRepository
+    {
+        private readonly IMongoCollection<ProductReview> _collection;
+        public ProductReviewRepository(IMongoClient client)
+        {
+            var database = client.GetDatabase("booksdb");
+            _collection = database.GetCollection<ProductReview>("reviews");
+        }
+        public async Task AddAsync(ProductReview productReview)
+        {
+            await _collection.InsertOneAsync(productReview);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _collection.DeleteOneAsync(r => r.Id == id);
+        }
+
+        public async Task<IEnumerable<ProductReview>> GetAllAsync()
+        {
+            return await _collection.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<ProductReview?> GetByIdAsync(string id)
+        {
+            return await _collection.Find(r => r.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(ProductReview productReview)
+        {
+            await _collection.ReplaceOneAsync(r => r.Id == productReview.Id, productReview);
+        }
+    }
+}
