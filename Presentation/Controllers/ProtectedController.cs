bfe73a7ec40637ed354922344728ed00eb7eecc1
punkmyduck <punkmyduck@mail.ru>
2025-10-28 17:27:53 +0300

    Добавлен метод в контроллер, позволяющий получить информацию о текущем пользователе

--- Presentation/Controllers/ProtectedController.cs
+++ Presentation/Controllers/ProtectedController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace task_1135.Presentation.Controllers
@@ -17,6 +19,20 @@ namespace task_1135.Presentation.Controllers
         public async Task<IActionResult> GetRestrictedContent()
         {
             return Ok(new { Message = "Access granted : older than 18" });
+        }
+
+        [HttpGet("me")]
+        [Authorize]
+        public async Task<IActionResult> GetCurrentUser()
+        {
+            var userName = User.Identity?.Name;
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return Ok(new
+            {
+                Id = userId,
+                UserName = userName,
+            });
         }
     }
 }
