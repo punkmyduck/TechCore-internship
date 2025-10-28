64fa42da0e05b95a20d97235d598ba5f93225b24
punkmyduck <punkmyduck@mail.ru>
2025-10-28 17:23:57 +0300

    Добавлен контроллер с защищенным политикой методом

---
+++ Presentation/Controllers/ProtectedController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace task_1135.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProtectedController : ControllerBase
+    {
+        public ProtectedController()
+        {
+            
+        }
+
+        [HttpGet("restricted")]
+        [Authorize(Policy = "OlderThan18")]
+        public async Task<IActionResult> GetRestrictedContent()
+        {
+            return Ok(new { Message = "Access granted : older than 18" });
+        }
+    }
+}
