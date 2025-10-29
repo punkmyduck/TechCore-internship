4a59ebb85209783d27c6a3ec1c2c52bfe55b7ee2
punkmyduck <punkmyduck@mail.ru>
2025-10-29 20:25:45 +0300

    Реализация gRPC клиент сервиса Book

--- BookGrpcService/Program.cs
+++ BookGrpcService/Program.cs
@@ -4,11 +4,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddSingleton<BookGrpcService.Services.BookService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+
+
+app.MapGet("/", async (BookGrpcService.Services.BookService bookService) =>
+{
+    await bookService.GetAuthorInfoAsync(1);
+    return "BookGrpcService called AuthorGrpcService";
+});
 
 app.Run();
