b49fca567f3e1f7e415a8df41dacba74af529cb1
punkmyduck <punkmyduck@mail.ru>
2025-10-29 12:41:48 +0300

    Добавлен тест с [Theory]

--- Tests/FirstUnitTest.cs
+++ Tests/FirstUnitTest.cs
@@ -13,5 +13,16 @@ namespace Tests
 
             Assert.Equal(5, result);
         }
+
+        [Theory]
+        [InlineData(-1,1)]
+        [InlineData(-5,5)]
+        [InlineData(0,0)]
+        [InlineData(10,10)]
+        public void Abs_ReturnsAbsoluteValue_ForVariousNumbers(int input, int expected)
+        {
+            int result = Math.Abs(input);
+            Assert.Equal(expected, result);
+        }
     }
 }
