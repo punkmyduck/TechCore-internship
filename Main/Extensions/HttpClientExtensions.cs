7501c5a5ad4648b681d0d251a1f701c3b8f14cc6
punkmyduck <punkmyduck@mail.ru>
2025-10-29 18:59:59 +0300

    Добавлен Polly, добавлена политика работы при ошибках при обращении к внешнему API

--- Main/Extensions/HttpClientExtensions.cs
+++ Main/Extensions/HttpClientExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace task_1135.Extensions
+﻿using Polly;
+
+namespace task_1135.Extensions
 {
     public static class HttpClientExtensions
     {
@@ -8,7 +10,8 @@
             {
                 client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
                 client.Timeout = TimeSpan.FromSeconds(10);
-            });
+            })
+                .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(1)));
         }
     }
 }
