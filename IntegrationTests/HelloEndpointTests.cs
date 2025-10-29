e820e2c25b6b6face2ac29f261ead9c16878b533
punkmyduck <punkmyduck@mail.ru>
2025-10-29 13:49:01 +0300

    Добавлен интеграционный тест для HelloController

---
+++ IntegrationTests/HelloEndpointTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using task_1135;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class HelloEndpointTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public HelloEndpointTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetHello_ReturnsOk()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/Hello");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
