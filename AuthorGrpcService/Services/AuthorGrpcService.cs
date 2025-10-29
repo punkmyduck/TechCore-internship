15f87ad54a411b93c033305dbcdf479b76ca481b
punkmyduck <punkmyduck@mail.ru>
2025-10-29 20:04:35 +0300

    Реализация gRPC сервиса Author

---
+++ AuthorGrpcService/Services/AuthorGrpcService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+
+namespace AuthorGrpcService.Services
+{
+    public class AuthorGrpcServiceImpl : AuthorService.AuthorServiceBase
+    {
+        public override Task<AuthorReply> GetAuthor(AuthorRequest request, ServerCallContext context)
+        {
+            var author = new AuthorReply
+            {
+                Id = request.Id,
+                Name = "Some dude"
+            };
+
+            return Task.FromResult(author);
+        }
+    }
+}
