99d8e8f82eaa0973ddd07fc6825ee22c2b463087
punkmyduck <punkmyduck@mail.ru>
2025-10-29 12:38:32 +0300

    Добавлен первый тест

---
+++ Tests/FirstUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    public class FirstUnitTest
+    {
+        [Fact]
+        public void Abs_ReturnsAbsoluteNumber_WhenNegativeNumber()
+        {
+            int b = -5;
+
+            int result = Math.Abs(b);
+
+            Assert.Equal(5, result);
+        }
+    }
+}
