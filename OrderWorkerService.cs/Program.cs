2e164a74c218869f4205deda5a60d6c5acb462ea
punkmyduck <punkmyduck@mail.ru>
2025-10-30 13:12:12 +0300

    Реализован SubmitOrderConsumer, принимающий сообщения из rabbitmq

--- OrderWorkerService.cs/Program.cs
+++ OrderWorkerService.cs/Program.cs
@@ -1,3 +1,6 @@
+using MassTransit;
+using OrderWorkerService.cs.Workers;
+
 namespace OrderWorkerService.cs
 {
     public class Program
@@ -6,6 +9,23 @@ namespace OrderWorkerService.cs
         {
             var builder = Host.CreateApplicationBuilder(args);
             builder.Services.AddHostedService<Worker>();
+
+            builder.Services.AddMassTransit(x =>
+            {
+                x.AddConsumer<SubmitOrderConsumer>();
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host("localhost", "/", h =>
+                    {
+                        h.Username("rabbit");
+                        h.Password("rabbitpass");
+                    });
+                    cfg.ReceiveEndpoint("submit-order-queue", e =>
+                    {
+                        e.ConfigureConsumer<SubmitOrderConsumer>(context);
+                    });
+                });
+            });
 
             var host = builder.Build();
             host.Run();
---
+++ OrderWorkerService.cs/Workers/SubmitOrderConsumer.cs
@@ -0,0 +1,21 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace OrderWorkerService.cs.Workers
+{
+    public class SubmitOrderConsumer : IConsumer<SubmitOrderCommand>
+    {
+        private readonly ILogger<SubmitOrderCommand> _logger;
+        public SubmitOrderConsumer(ILogger<SubmitOrderCommand> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<SubmitOrderCommand> context)
+        {
+            var msg = context.Message;
+            _logger.LogInformation($"Order received: {msg.OrderId} with {msg.Items.Count} items");
+            return Task.CompletedTask;
+        }
+    }
+}
