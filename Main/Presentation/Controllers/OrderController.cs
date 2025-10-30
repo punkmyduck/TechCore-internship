511661c56bdb06b1b44f59e47313df26e1ca32dd
punkmyduck <punkmyduck@mail.ru>
2025-10-30 12:57:08 +0300

    Реализация контроллера для отправки сообщения по заказу

---
+++ Main/Presentation/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace task_1135.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IBus _bus;
+        public OrderController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Submit([FromBody] List<LineItem> items)
+        {
+            var command = new SubmitOrderCommand(Guid.NewGuid(), items);
+            await _bus.Publish(command);
+            return Accepted();
+        }
+    }
+}
