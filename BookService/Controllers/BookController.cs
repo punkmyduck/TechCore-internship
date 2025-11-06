a515bf3235a0bd294bd5b7c659091eb22c3737a6
punkmyduck <punkmyduck@mail.ru>
2025-11-06 16:57:14 +0300

    Создан упрощенный BookService, удалены предыдущение kubernetes файлы

---
+++ BookService/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        public BookController()
+        {
+            
+        }
+
+
+    }
+}
